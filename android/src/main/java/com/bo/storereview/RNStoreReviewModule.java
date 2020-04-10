9f99bb6d10d99b51a4ede3819289243e67c3d528
WheelerLee <490386516@qq.com>
2020-04-10 10:51:31 +0800

    android

---
+++ android/src/main/java/com/bo/storereview/RNStoreReviewModule.java
@@ -0,0 +1,37 @@
+package com.bo.storereview;
+
+import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
+import android.text.TextUtils;
+
+import com.facebook.react.bridge.Promise;
+import com.facebook.react.bridge.ReactApplicationContext;
+import com.facebook.react.bridge.ReactContextBaseJavaModule;
+import com.facebook.react.bridge.ReactMethod;
+
+import javax.annotation.Nonnull;
+
+public class RNStoreReviewModule extends ReactContextBaseJavaModule {
+
+    public RNStoreReviewModule(@Nonnull ReactApplicationContext reactContext) {
+        super(reactContext);
+    }
+
+    @Nonnull
+    @Override
+    public String getName() {
+        return "StoreReview";
+    }
+
+    @ReactMethod
+    public void show(String package) {
+
+        Uri uri = Uri.parse("market://details?id=" + package);
+        Intent intent = new Intent(Intent.ACTION_VIEW, uri);
+        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        getCurrentActivity().startActivity(intent);
+
+    }
+
+}
